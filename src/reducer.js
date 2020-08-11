de07752b4844dd85708da572bdf44d1abda59729
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-08-11 14:49:24 +0300

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      const addGood = { 
+        ...state, 
+        good: state.good + 1
+      }
+      
+      return addGood
+    case 'OK':
+      const addOk = { 
+        ...state, 
+        ok: state.ok + 1
+      }
+      return addOk
+    case 'BAD':
+      const addBad = { 
+        ...state, 
+        bad: state.bad + 1
+      }
+      return addBad
+    case 'ZERO':
+      return initialState
+    default: return initialState
+  }
+  
+}
+
+export default counterReducer
